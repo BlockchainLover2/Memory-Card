5bf9bb6b5d159147c981b628a36dbc3776aeb466
BlockchainLover2 <batuyek442@gmail.com>
2024-07-29 13:31:38 +0300

    Random Pokemon Added
    People can solve easily without doing random

--- src/ImageApi.js
+++ src/ImageApi.js
@@ -3,10 +3,14 @@ export async function getImages(){
 
 
     for (let i = 1; i < 10; i++) {
-        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`,{mode: 'cors'})
+        let random = getRandomInt(1,100);
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${random}`,{mode: 'cors'})
         const data = await response.json();
         imageArray.push({name:data.name,img:data.sprites.back_default,sound:new Audio(data.cries.latest)})
     }
     return imageArray;
 
+}
+function getRandomInt(min,max) {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
